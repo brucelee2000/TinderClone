5ef0f4a72c3d0fc0326e8119ebd7da84d3d2843f
brucelee2000 <brucelee2000@tp.edu.tw>
2015-02-12 21:32:01 -0800

    Facebook framework setup including adding SDK, configure .plist and create bridging header file.

---
+++ TinderClone/TinderClone-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  TinderClone-Bridging-Header.h
+//  TinderClone
+//
+//  Created by Yosemite on 2/12/15.
+//  Copyright (c) 2015 Yosemite. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
+

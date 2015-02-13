55ee5c26348088283b5dc1022c93af04ec9389aa
brucelee2000 <brucelee2000@tp.edu.tw>
2015-02-12 22:22:52 -0800

    Parse setup for Facebook: details in Parse doc.

---
+++ TinderClone-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  TinderClone-Bridging-Header.h
+//  TinderClone
+//
+//  Created by Yosemite on 2/12/15.
+//  Copyright (c) 2015 Yosemite. All rights reserved.
+//
+
+#ifndef TinderClone_TinderClone_Bridging_Header_h
+#define TinderClone_TinderClone_Bridging_Header_h
+#import <FacebookSDK/FacebookSDK.h>
+#import <Parse/Parse.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
+
+#endif
--- TinderClone/AppDelegate.swift
+++ TinderClone/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Initialize facebook Parse plugin
+        PFFacebookUtils.initializeFacebook()
+        //Connect to Parse
+        Parse.setApplicationId("IXcFpI577bBpo0SxQFAA16VGDY7KdkzDUXMeFBkS", clientKey: "T8RHEjrYnlXh4wMwNz5JUnBYvz20735dz2wnbOMT")
+        
         return true
     }
 
@@ -33,14 +39,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication,withSession:PFFacebookUtils.session())
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        FBAppCall.handleDidBecomeActiveWithSession(PFFacebookUtils.session())
+    }
 
 }
 
